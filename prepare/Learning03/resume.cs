8daffa408b7a5ed3db89d58dd9b5211b47f3c596
TheRepublicCommando <bfgglenn@gmail.com>
2023-01-18 19:34:56 -0800

    ree...jobs

---
+++ prepare/Learning03/resume.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+    public List<job> _jobs = new List<job>();
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (job job in _jobs)
+        {
+            job.display();
+        }
+    }
+}
