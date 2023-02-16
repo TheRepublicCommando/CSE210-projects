e8f1918e6503564e5e71b92e0763b9b7aeefd9a0
TheRepublicCommando <bfgglenn@gmail.com>
2023-02-15 21:39:06 -0800

    hmmm

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,55 @@
 using System;
 
-class Program
+public class Assignment
 {
-    static void Main(string[] args)
+    private string _studentName;
+    private string _topic;
+    public Assignment(string studentName, string topic)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    public string GetTopic()
+    {
+        return _topic;
+    }
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        _title = title;
+    }
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
     }
 }
